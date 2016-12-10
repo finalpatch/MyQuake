3762a6861e9c865f496bfa2bf70a6c2db7ce31fe
finalpatch <fengil@gmail.com>
2016-12-10 23:42:33 +1100

    add music

--- src/cd_sdl.c
+++ src/cd_sdl.c
@@ -18,24 +18,35 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
 #include "quakedef.h"
+#include <SDL2/SDL_mixer.h>
+
+static Mix_Music* tracks[12];
 
 void CDAudio_Play(byte track, qboolean looping)
 {
+	if (track <= 11 && tracks[track])
+	{
+		Mix_PlayMusic(tracks[track], looping ? -1 : 1);
+	}
 }
 
 
 void CDAudio_Stop(void)
 {
+	Mix_PauseMusic();
+	Mix_RewindMusic();
 }
 
 
 void CDAudio_Pause(void)
 {
+	Mix_PauseMusic();
 }
 
 
 void CDAudio_Resume(void)
 {
+	Mix_ResumeMusic();
 }
 
 
@@ -46,10 +57,30 @@ void CDAudio_Update(void)
 
 int CDAudio_Init(void)
 {
+	char filename[32];
+
+	Mix_Init(MIX_INIT_OGG);
+	Mix_OpenAudio(44100, AUDIO_S16, 2, 4096);
+
+	for (int i = 2; i < 12; ++i)
+	{
+		sprintf(filename, "id1/music/track%02d.ogg", i);
+		tracks[i] = Mix_LoadMUS(filename);
+	}
+
+	Mix_VolumeMusic(MIX_MAX_VOLUME);
+
 	return 0;
 }
 
 
 void CDAudio_Shutdown(void)
 {
+	for (int i = 2; i < 12; ++i)
+	{
+		Mix_FreeMusic(tracks[i]);
+		tracks[i] = NULL;
+	}
+	Mix_CloseAudio();
+	Mix_Quit();
 }
