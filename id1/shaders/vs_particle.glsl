1ac45d8112185fe61967eabbd3f88ac3800b9a6f
finalpatch <fengil@gmail.com>
2017-01-26 00:30:44 +1100

    size

--- id1/shaders/vs_particle.glsl
+++ id1/shaders/vs_particle.glsl
@@ -10,10 +10,6 @@ layout(std140) uniform UniformBlock
 layout (location = 0) in vec4 position;
 layout (location = 1) in vec4 color;
 
-#define MIN_PARTICLE_SIZE 0.0625
-#define MAX_PARTICLE_SIZE 1.0
-#define MAX_DISTANCE 1000
-
 out VS_OUT
 {
     vec4 color;
@@ -26,7 +22,5 @@ void main(void)
     vs_out.color = color;
 
     float dist = distance(uniforms.origin.xyz, position.xyz);
-    float scale = 1.0 - dist / MAX_DISTANCE;
-    scale = clamp(scale, MIN_PARTICLE_SIZE, MAX_PARTICLE_SIZE);
-    gl_PointSize = 16 * scale;
+    gl_PointSize = 1024.0 / dist;
 }
