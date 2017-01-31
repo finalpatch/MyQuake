9106bb19afa68c2cd51650f4e99b0cdf77fbab1e
finalpatch <fengil@gmail.com>
2017-02-01 00:17:07 +1100

    add stencil buffer

--- src/ren_gl/vid_gl.cpp
+++ src/ren_gl/vid_gl.cpp
@@ -43,6 +43,7 @@ void VID_Init (unsigned char *palette)
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE); 
 
     win = SDL_CreateWindow("Quake", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
